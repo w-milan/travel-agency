2f8c30822ec5b385addee85ba18d6252efd91c4a
milan <pperisic.milan@gmail.com>
2022-10-26 00:28:01 +0200

    Added few more locations

--- src/data.js
+++ src/data.js
@@ -27,21 +27,34 @@ export default [
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     },
     {
-        title: "Sidney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/U46R7t4gxiJTYNZu9",
-        startDate: "21 Sep, 2021",
-        endDate: "19 Oct, 2021",
-        description: "Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.",
-        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+        title: "Uvac",
+        location: "Serbia",
+        googleMapsUrl: "https://goo.gl/maps/WMi9SivePZL7C8Wb9",
+        startDate: "21 Sep, 2022",
+        endDate: "19 Dec, 2022",
+        description: "Special nature reserve “Uvac”, premier category natural estate of great importance, is located on south western Serbia",
+        imageUrl: "https://source.unsplash.com/0Zul3ftxUII"
     },
     {
-        title: "Geirangerfjord",
-        location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/Xr9tH8f6DX9KMmhn8",
-        startDate: "05 Jun, 2022",
-        endDate: "11 Jul, 2022",
-        description: "Iconic fjord offering snowy mountain peaks, lush plant life, cascading falls & spectacular views.",
-        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+        title: "Saint Basil's Cathedral",
+        location: "Rusia",
+        googleMapsUrl: "https://goo.gl/maps/jEAnabr6rA1gqevVA",
+        startDate: "05 Feb, 2022",
+        endDate: "11 Mar, 2022",
+        description: "The Cathedral of Vasily the Blessed, known as Saint Basil's Cathedral, is an Orthodox church in Red Square of Moscow.",
+        imageUrl: "https://source.unsplash.com/xSQ3VmvqUN4"
+    },
+    {
+        title: "The Great Pyramid of Giza",
+        location: "Egypt",
+        googleMapsUrl: "https://goo.gl/maps/6nFibYBM1xAJRF466",
+        startDate: "25 Jul, 2022",
+        endDate: "15 Aug, 2022",
+        description: "Largest of Egyptian pyramids in a complex also including temples & other Ancient artifacts.",
+        imageUrl: "https://source.unsplash.com/ONVA6s03hg8"
     }
 ]
+
+
+
+
