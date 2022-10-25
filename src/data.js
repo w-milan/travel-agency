af23ef997c83ba63ea51a67b306395b9ad0f6f22
milan <pperisic.milan@gmail.com>
2022-10-26 00:12:17 +0200

    Fixing design for more cards

--- src/data.js
+++ src/data.js
@@ -25,5 +25,23 @@ export default [
         endDate: "11 Jul, 2022",
         description: "Iconic fjord offering snowy mountain peaks, lush plant life, cascading falls & spectacular views.",
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
+    },
+    {
+        title: "Sidney Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/U46R7t4gxiJTYNZu9",
+        startDate: "21 Sep, 2021",
+        endDate: "19 Oct, 2021",
+        description: "Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.",
+        imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
+    },
+    {
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/Xr9tH8f6DX9KMmhn8",
+        startDate: "05 Jun, 2022",
+        endDate: "11 Jul, 2022",
+        description: "Iconic fjord offering snowy mountain peaks, lush plant life, cascading falls & spectacular views.",
+        imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     }
 ]
