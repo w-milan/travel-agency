78579f66c27ff52a4808d5a2478ba871890d1ffe
milan <pperisic.milan@gmail.com>
2022-10-26 00:05:47 +0200

    Changing data

--- src/data.js
+++ src/data.js
@@ -5,25 +5,25 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        description: "Iconic snow-capped peak & volcano, the highest in Japan & the 7th highest island peak on earth.",
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
         title: "Sidney Opera House",
         location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        googleMapsUrl: "https://goo.gl/maps/U46R7t4gxiJTYNZu9",
+        startDate: "21 Sep, 2021",
+        endDate: "19 Oct, 2021",
+        description: "Landmark, skyline-dominating arts centre for opera, theatre, music and dance, plus guided tours.",
         imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
     },
     {
         title: "Geirangerfjord",
         location: "Norway",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        googleMapsUrl: "https://goo.gl/maps/Xr9tH8f6DX9KMmhn8",
+        startDate: "05 Jun, 2022",
+        endDate: "11 Jul, 2022",
+        description: "Iconic fjord offering snowy mountain peaks, lush plant life, cascading falls & spectacular views.",
         imageUrl: "https://source.unsplash.com/3PeSjpLVtLg"
     }
 ]
