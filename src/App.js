5d8b0da68a97873108e46b5dd7fd16fbca6d5c19
milan <pperisic.milan@gmail.com>
2022-10-25 22:54:27 +0200

    Set props and pick data from file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
+import data from "./data";
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+        key={item.id}
+        item={item}
+      />
+    )
+  })
+
   return (
     <div className="App">
       <>
         <Navbar />
-        <Card />
+        <section>
+          {cards}
+        </section>
       </>
     </div>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div>
       <div>
-        <img src="https://source.unsplash.com/WLxQvbMyfas" alt="" />
-        <p>Japan</p>
-        <a href="">Visit on Google Maps</a>
+        <img src={props.item.imageUrl} alt="" />
+        <p>{props.item.location}</p>
+        <a href={props.item.googleMapsUrl}>Visit on Google Maps</a>
       </div>
-      <h1>Mount Fuji</h1>
-      <p><strong>12 Jan, 2021 - 24Jan, 2021</strong></p>
-      <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists</p>
+      <h1>{props.item.title}</h1>
+      <p><strong>{props.item.startDate} - {props.item.endDate}</strong></p>
+      <p>{props.item.description}</p>
     </div>
   )
 }
 
 export default Card
+
