9702512557e63e9b577f1806e88f37a9f25260d0
milan <pperisic.milan@gmail.com>
2022-10-25 22:41:31 +0200

    Added Navbar and Card Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
       <>
-        <h1>App</h1>
+        <Navbar />
+        <Card />
       </>
     </div>
   );
---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div>
+        <img src="https://source.unsplash.com/WLxQvbMyfas" alt="" />
+        <p>Japan</p>
+        <a href="">Visit on Google Maps</a>
+      </div>
+      <h1>Mount Fuji</h1>
+      <p><strong>12 Jan, 2021 - 24Jan, 2021</strong></p>
+      <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists</p>
+    </div>
+  )
+}
+
+export default Card
